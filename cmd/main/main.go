41e223f1910b64b4d70a570d1f0ac553ef1c93cb
leonardodfelix <leonardo.doria.felix1983@gmail.com>
2022-07-21 20:29:58 -0300

    add main server

---
+++ cmd/main/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/leonardodfelix/mysql-crud/pkg/routes"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterUserRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:9010", r))
+}
